01486d4bab5953fd324da1e062997038da639617
Heng-Yi Lin <b98611008@ntu.edu.tw>
2015-10-06 03:46:53 +0800

    Update PercolationStats.java

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -22,7 +22,6 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
 	
-	private Percolation[] testGrid;
 	private double[] probability;
 	
 	/**
@@ -33,22 +32,22 @@ public class PercolationStats {
 	 * @throws IllegalArgumentException if N or T <= 0
 	 */
 	public PercolationStats(int N, int T) {
-		if(N > 0 && T > 0) { // perform T exps with N*N grid if N(grid size) and T(time of experiments) > 0 
-			testGrid = new Percolation[T]; // init array to store model for T experiments
+		if (N > 0 && T > 0) { // perform T exps with N*N grid if N(grid size) and T(time of experiments) > 0 
+			Percolation testGrid; // init array to store model for T experiments
 			probability = new double[T]; // init array to store p for T experiments
-			int row,column,openSitesNum; // row and column for site, and number of open sites 
-			for(int i = 0; i < T; i++) {
-				testGrid[i] = new Percolation(N); // init new N*N grid model
+			int row, column, openSitesNum; // row and column for site, and number of open sites 
+			for (int i = 0; i < T; i++) {
+				testGrid = new Percolation(N); // init new N*N grid model
 				openSitesNum = 0; // init count for number of open sites
-				do{ // keep running till it percolates
-					row = StdRandom.uniform(1, N+1); // assign random row in [1, N+1)
-					column = StdRandom.uniform(1, N+1); // assign random column in [1, N+1)
-					if(!testGrid[i].isOpen(row, column)){ //if the site isn't open
-						testGrid[i].open(row, column); // open site(row, column)
+				do { // keep running till it percolates
+					row = StdRandom.uniform(1, N + 1); // assign random row in [1, N+1)
+					column = StdRandom.uniform(1, N + 1); // assign random column in [1, N+1)
+					if (!testGrid.isOpen(row, column)) { //if the site isn't open
+						testGrid.open(row, column); // open site(row, column)
 						openSitesNum++; // number of open sites + 1
 					}
-				}while(!testGrid[i].percolates());
-				probability[i] = ((double)openSitesNum/(N*N)); // save probability to array for each experiment				
+				} while(!testGrid.percolates());
+				probability[i] = ((double) openSitesNum / (N * N)); // save probability to array for each experiment				
 			}
 		} else // throw IllegalArgumentException if N or T <= 0
 			throw new IllegalArgumentException("Both N and T should be greater than 0 (exclusive)");
@@ -99,17 +98,16 @@ public class PercolationStats {
 	 * 				 T: times of experiments
 	 */
 	public static void main(String[] args) {
-		int N = 2; // default 2*2 grid
-		int T = 30; // default 30 times
-		if(args.length == 2){
+		int N = 100; // default 2*2 grid
+		int T = 16; // default 30 times
+		if (args.length == 2) {
 			N = Integer.parseInt(args[0]);
 			T = Integer.parseInt(args[1]);
 		}
-		PercolationStats MonteCarloSimulation = new PercolationStats(N, T);
-		StdOut.println("mean\t\t\t= " + MonteCarloSimulation.mean());
-		StdOut.println("stddev\t\t\t= " + MonteCarloSimulation.stddev());
-		StdOut.println("95% confidence interval = " + MonteCarloSimulation.confidenceLo() + ", " + MonteCarloSimulation.confidenceHi());
-		
+		PercolationStats MonteCarloSim = new PercolationStats(N, T);
+		StdOut.println("mean\t\t\t= " + MonteCarloSim.mean());
+		StdOut.println("stddev\t\t\t= " + MonteCarloSim.stddev());
+		StdOut.println("95% confidence interval = " + MonteCarloSim.confidenceLo() + ", " + MonteCarloSim.confidenceHi());
 	}
 }
 
